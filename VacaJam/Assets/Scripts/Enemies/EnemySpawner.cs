2bc4d84d067f7483797b5ec200ee63b77b5349ec
gseven-gdv <zerogravitystd@gmail.com>
2021-10-16 17:15:47 -0300

    muita gambiarra kk

--- VacaJam/Assets/Scripts/Enemies/EnemySpawner.cs
+++ VacaJam/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -8,9 +8,13 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Wave[] _waves;
     [SerializeField] private float _nextWaveCooldown;
+    [SerializeField] private Vector2 _maxSpawnOffset;
+    [SerializeField] private Vector2 _minSpawnOffset;
 
     private int _waveCount = 0;
     private int _enemiesSpawned = 0;
+
+    private int can;
 
     private float _nextSpawn;
 
@@ -22,17 +26,17 @@ public class EnemySpawner : MonoBehaviour
 
         public GameObject[] Enemies => _enemies;
         public float SpawnRate => _spawnRate;
-        public int EnemiesAmount => _enemiesAmount;
+        public int EnemiesAmount => _enemiesAmount + 1;
     }
 
     private void Update()
     {
-        if (_waveCount >= _waves.Length) {
+        if (_waveCount > _waves.Length - 1) {
             Destroy(this.gameObject);
 
             return;
         }
-
+        
         SpawnEnemies();
     }
 
@@ -40,20 +44,33 @@ public class EnemySpawner : MonoBehaviour
     {
         Wave wave = _waves[_waveCount];
 
-        if (Time.time >= _nextSpawn && _enemiesSpawned <= wave.EnemiesAmount - 1) {
-            Instantiate(wave.Enemies[Random.Range(0, wave.Enemies.Length - 1)], transform.position, Quaternion.identity);
+        if (Time.time >= _nextSpawn && _enemiesSpawned < wave.EnemiesAmount) {
+            Instantiate(
+                original: wave.Enemies[Random.Range(0, wave.Enemies.Length - 1)],
+                position: (Vector2) transform.position + new Vector2(
+                            Random.Range(_minSpawnOffset.x, _maxSpawnOffset.x), Random.Range(_minSpawnOffset.y, _maxSpawnOffset.y)
+                          ),
+                rotation: Quaternion.identity
+            );
 
             _enemiesSpawned++;
             _nextSpawn = Time.time + 1f / wave.SpawnRate;
         }
-        else if (_enemiesSpawned > wave.EnemiesAmount - 1) {
+
+        if (_enemiesSpawned >= wave.EnemiesAmount) {
             StartCoroutine(NextWave(_nextWaveCooldown));
+            can = 0;
         }
     }
 
     private IEnumerator NextWave(float time)
     {
         yield return new WaitForSeconds(time);
+
+        can++;
+
+        if (can > 1)
+            yield break;
 
         _waveCount++;
     }
